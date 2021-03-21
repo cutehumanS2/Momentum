0aff98d46f7dea1d8b40f090fecb628ab4bba80f
cutehumanS2 <ganzi_girl22@naver.com>
2021-03-21 09:15:26 +0900

    3.2

--- clock.js
+++ clock.js
@@ -8,11 +8,14 @@ function getTime(){
     const hours = date.getHours();
     const seconds = date.getSeconds();
     //innerText ~> 객체 안에 텍스트 넣음 / ``을 '백틱'이라 함
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+            minutes <10 ? `0${minutes}` : minutes} : ${
+            seconds <10 ? `0${seconds}` : seconds}`;
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000); //getTime()을 1초마다 수행
 }
 
 init();
